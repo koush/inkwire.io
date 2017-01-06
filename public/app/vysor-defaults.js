8e8c16d8bf0c1aeb51d21a25513fbfef12c5e585
Koushik Dutta <koushd@gmail.com>
2017-01-05 16:30:45 -0800

    update

--- public/app/vysor-defaults.js
+++ public/app/vysor-defaults.js
@@ -1,11 +1,15 @@
 var vysorDefaultSettings =
 // copy everything below into the settings editor. must be valid JSON.
 {
+  "mousedown": {
+    "1": "KEYCODE_HOME",
+    "2": "KEYCODE_BACK"
+  },
   "keydown": {
-    "ArrowDown": "KEYCODE_DOWN",
-    "ArrowLeft": "KEYCODE_LEFT",
-    "ArrowRight": "KEYCODE_RIGHT",
-    "ArrowUp": "KEYCODE_UP",
+    "ArrowDown": "KEYCODE_DPAD_DOWN",
+    "ArrowLeft": "KEYCODE_DPAD_LEFT",
+    "ArrowRight": "KEYCODE_DPAD_RIGHT",
+    "ArrowUp": "KEYCODE_DPAD_UP",
     "Backspace": "KEYCODE_DEL",
     "Delete": "KEYCODE_FORWARD_DEL",
     "Enter": "KEYCODE_ENTER",
