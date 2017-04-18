a918ca4e7c56f9a6f8b34c733444a766cd998251
Koushik Dutta <koushd@gmail.com>
2017-04-18 12:11:32 -0700

    fixups

--- app.js
+++ app.js
@@ -52,11 +52,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-var debug = require('debug')('inkwire-site');
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
